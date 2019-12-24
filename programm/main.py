4f9607e8416f6b35501699e6afc89fb319cf8ce8
afanse <darkserg89@mail.ru>
2019-12-24 20:37:27 +0300

    24122019

--- programm/main.py
+++ programm/main.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
-
-
+#import routeros_api
+import string
+import random
+import smtplib
+import routeros
 from mailer import send_mail
+
+mail_message = '''
+Hello, The Password on the Guest WIFI has been changed.
+SSID: MBMFWF
+Password: {}
+'''
+
+def generate_pass(length=8):
+	'Generate password with the length'
+	symbols=string.ascii_uppercase + string.ascii_lowercase + string.digits
+	return ''.join(random.choices(symbols,k=length))
+
+
+
+	
+if __name__=="__main__":
+	print(generate_pass())	
